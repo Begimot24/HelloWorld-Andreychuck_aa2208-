1887834c374a152d81dbd5803413dcb2e545d151
Artem <a89226549107@icloud.com>
2023-09-14 22:53:39 +0300

    project template

---
+++ Hello_World.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double value;
+
+	cout << "Hello World!\n";
+
+	cout << "Please enter a number: ";
+
+	cin >> value;
+
+	cout << "Your number is ";
+
+	cout << value;
+
+	return 0;
+}
